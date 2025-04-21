f5fc632f90220a7fec1ae544dde01f305c88e187
Rew Exo <rew@rewdeb.exo>
2025-04-21 10:49:29 -0700

    Added a second PriQueue test

--- test/priqueue_tests.cpp
+++ test/priqueue_tests.cpp
@@ -43,5 +43,28 @@ TEST(PriQueueTests, TestPushPop)
 
 TEST(PriQueueTests, TestErase)
 {
+    PriQueue<event, eventCompare> pq;
+    int count = 100000;
 
+    std::vector<event*> events;
+    for (int i = 0; i < count; i++) {
+        event* e = new event({i, nullptr});
+        events.push_back(e);
+        pq.push(e);
+    }
+
+    // erase odd elements
+    for (int i = 1; i < count; i+=2) {
+        pq.erase(events[i]);
+    }
+
+    for (int i = 0; i < count/2; i++) {
+        EXPECT_GE(pq.top()->value % 2, 0);
+        pq.pop();
+    }
+    EXPECT_EQ(pq.empty(), true);
+
+    for (int i = 0; i < count; i++) {
+        delete events[i];
+    }
 }
